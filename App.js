1566e5901f61d0bafcfdd4cbeaae93ab3b258495
ndrini <aless@ndrini.eu>
2023-03-13 07:08:17 +0100

    feat: implemented the pseudo routing switch add first compoment (just a placeholder) and implement the switch statement into App.js file

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+
+import PlaceholderPage from './components/PlaceholderPage';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [page, setPage] = useState("main")
+
+  switch (page) {
+    case "main":
+      return (
+          <PlaceholderPage
+            page={"main"}
+          />
+
+      );
+    case "credits":
+      return (
+        <PlaceholderPage
+          page={"credits"}
+        />
+      );
+    default:
+      break;
+        // code block
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
